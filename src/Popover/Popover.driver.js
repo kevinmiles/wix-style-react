a70555cdbde07e1dd80417232825308960122dd0
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-12-09 18:14:10 +0200

    <Popover/> - update drivers and css

--- src/Popover/Popover.driver.js
+++ src/Popover/Popover.driver.js
@@ -1,13 +1,8 @@
 import { popoverDriverFactory as corePopoverDriverFactory } from 'wix-ui-core/drivers/vanilla';
 
 const popoverDriverFactory = ({ element, eventTrigger }) => {
-  const driver = {
-    exists: () => !!element,
-  };
-
   return {
     ...corePopoverDriverFactory({ element, eventTrigger }),
-    ...driver,
   };
 };
 
--- src/Popover/Popover.protractor.driver.js
+++ src/Popover/Popover.protractor.driver.js
@@ -1,13 +1,8 @@
 import { popoverDriverFactory as corePopoverDriverFactory } from 'wix-ui-core/dist/src/components/Popover/Popover.protractor.driver';
 
 const popoverDriverFactory = component => {
-  const driver = {
-    exists: () => !!component,
-  };
-
   return {
     ...corePopoverDriverFactory(component),
-    ...driver,
   };
 };
 
