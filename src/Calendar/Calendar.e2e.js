ba15fd503caa7207222bf0a77170ed3e66ee6fb2
Tal Mardix <talma@wix.com>
2018-12-11 19:05:57 +0200

    Calendar multi month support

--- src/Calendar/Calendar.e2e.js
+++ src/Calendar/Calendar.e2e.js
@@ -7,14 +7,14 @@ import { createStoryUrl } from '../../test/utils/storybook-helpers';
 import { TESTS_PREFIX } from '../../stories/storiesHierarchy';
 
 describe('Calendar', () => {
-  const storyUrl = ({ selectedDays }) => {
+  const storyUrl = ({ selectedDays, numOfMonths }) => {
     const baseUrl = createStoryUrl({
       kind: `${TESTS_PREFIX}/3. Inputs/3.13 Calendar`,
       story: '1. selectedDays',
     });
     return `${baseUrl}&selectedDays=${global.encodeURIComponent(
       JSON.stringify(selectedDays),
-    )}`;
+    )}&numOfMonths=${numOfMonths || 1}`;
   };
   const dataHook = 'calendar';
   const driver = calendarTestkitFactory({ dataHook });
@@ -37,6 +37,23 @@ describe('Calendar', () => {
             from: new Date('2017/05/02'),
             to: new Date('2017/05/06'),
           },
+        }),
+      );
+      await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
+    },
+    { enableSnapshotAtBrowserGet: false },
+  );
+
+  eyes.it(
+    'should correctly render two months',
+    async () => {
+      await browser.get(
+        storyUrl({
+          selectedDays: {
+            from: new Date('2017/05/02'),
+            to: new Date('2017/05/06'),
+          },
+          numOfMonths: 2,
         }),
       );
       await waitForVisibilityOf(driver.getElement(), 'Cannot find Calendar');
